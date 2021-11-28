e8ff9a65bba0d68c9404170fdb406d53862ca2ea
Antonio Eloy <antonioeloy14@gmail.com>
2021-11-28 11:50:11 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/quadrinho/api/QuadrinhoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.quadrinho.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadrinhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadrinhoApiApplication.class, args);
+	}
+
+}
